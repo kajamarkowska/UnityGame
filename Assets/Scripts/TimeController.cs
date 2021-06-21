3ea7707a06b46c4b950c5bb2386f06b767239672
kaja.markowska <kaja.markowska@wp.pl>
2021-06-21 21:16:43 +0200

    Add new files

---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(10 * Time.deltaTime, 0f, 0f);
+    }
+}
